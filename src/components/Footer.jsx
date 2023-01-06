a6f42b3efff1647755c09a354e752cfe0c9fec04
ChangHorng <changhorng05@gmail.com>
2023-01-06 18:17:22 +0800

    Convert Footer's CSS to Tailwind CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react"
+import React from "react";
 import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from "react-icons/fa"
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <FaTwitterSquare className="footer-twitter"/>
-            <FaFacebookSquare className="footer-facebook"/>
-            <FaInstagramSquare className="footer-instagram"/>
-            <FaGithubSquare className="footer-github"/>
-        </footer>
-    )
+  return (
+    <footer className="mt-10 flex gap-6 rounded-b-[10px] bg-[#161619] pt-[19px] pb-5 pl-[73px] text-[#918E9B]">
+      <FaTwitterSquare className="h-[25px] w-[25px]" />
+      <FaFacebookSquare className="h-[25px] w-[25px]" />
+      <FaInstagramSquare className="h-[25px] w-[25px]" />
+      <FaGithubSquare className="h-[25px] w-[25px]" />
+    </footer>
+  )
 }
