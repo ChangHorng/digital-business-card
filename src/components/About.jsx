9534c6edac10ebf2888a6d2015eb5bae7c97124c
ChangHorng <changhorng05@gmail.com>
2023-01-06 18:16:26 +0800

    Convert About's CSS to Tailwind CSS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 
 export default function About() {
-    return (
-        <div className="about">
-            <h2>About</h2>
-            <p>I am a frontend developer with a particular interest 
-                in making things simple and automating daily tasks.
-                I try to keep up with security and best practices, 
-                and am always looking for new things to learn.</p>
-        </div>
-    )
+  return (
+    <div className="mt-[35px] ml-[36px] w-[247px] items-center">
+      <h2 className="mb-0 text-big font-bold text-milk-white">About</h2>
+      <p className="mt-1 text-small font-normal text-dark-white">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </div>
+  )
 }
