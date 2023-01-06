b06541ba3092dd8743004f641651cadcde13f444
ChangHorng <changhorng05@gmail.com>
2023-01-06 18:18:22 +0800

    Formatted code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer"
 
 export default function App() {
   return (
-    <div className="bg-[#1A1B21] rounded-[10px]">
+    <div className="rounded-[10px] bg-[#1A1B21]">
       <Info />
       <About />
       <Interest />
