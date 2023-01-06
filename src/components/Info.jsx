d8d7fdf45411ef9d5c09020a549088b8e5d06d58
ChangHorng <changhorng05@gmail.com>
2023-01-06 18:15:54 +0800

    Convert Info's CSS to Tailwind CSS and format code

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,22 +3,28 @@ import profile_pic from "../images/Chang Horng.png"
 import { FaEnvelope, FaLinkedin } from "react-icons/fa"
 
 export default function Info() {
-    return (
-        <div>
-            <img src={profile_pic} className="w-[317px] h-[317px] rounded-t-[10px]" />
-            <h1 className="mt-6 mb-0 font-bold text-big text-center text-white">Lee Chang Horng</h1>
-            <h3 className="mt-1 mb-0 font-normal text-medium text-center text-light-orange">Frontend Developer</h3>
-            <p className="mt-[9px] mb-0 font-normal text-small text-center text-milk-white">github.com/ChangHorng</p>
-            <div className="flex mt-4">
-                <button className="ml-[35px] mr-[17px] box-border flex justify-center items-center py-[9px] pl-[11px] pr-[13px] gap-2 w-[115px] h-[34px] bg-white border-[1px] border-solid border-[#D1D5DB] shadow-button rounded-md font-medium text-button">
-                    <FaEnvelope />  
-                    Email
-                </button>
-                <button className="flex justify-center items-center py-[9px] pl-[11px] pr-[13px] gap-2 w-[115px] h-[34px] text-white bg-[#5093E2] border-[1px] border-[#5093E2] shadow-button rounded-md font-medium text-button">
-                    <FaLinkedin />
-                    LinkedIn
-                </button>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <img src={profile_pic} className="h-[317px] w-[317px] rounded-t-[10px]" />
+      <h1 className="mt-[26px] mb-0 text-center text-extra-big font-bold text-white">
+        Lee Chang Horng
+      </h1>
+      <h3 className="mt-1 mb-0 text-center text-medium font-normal text-light-orange">
+        Frontend Developer
+      </h3>
+      <p className="mt-[9px] mb-0 text-center text-small font-normal text-milk-white">
+        github.com/ChangHorng
+      </p>
+      <div className="mt-4 flex">
+        <button className="ml-[35px] mr-[17px] box-border flex h-[34px] w-[115px] items-center justify-center gap-2 rounded-md border-[1px] border-solid border-[#D1D5DB] bg-white py-[9px] pl-[11px] pr-[13px] text-button font-medium shadow-button">
+          <FaEnvelope />
+          Email
+        </button>
+        <button className="flex h-[34px] w-[115px] items-center justify-center gap-2 rounded-md border-[1px] border-[#5093E2] bg-[#5093E2] py-[9px] pl-[11px] pr-[13px] text-button font-medium text-white shadow-button">
+          <FaLinkedin />
+          LinkedIn
+        </button>
+      </div>
+    </div>
+  )
 }
